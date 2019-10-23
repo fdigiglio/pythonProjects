bef2fd93f5040ab23072ea55115fb91b16f56ea2
Frank Di Giglio <fdigiglio22109@columbushs.com>
2019-10-22 22:31:31 -0400

    added multiplication working on sub and div fix

--- calculator/script.py
+++ calculator/script.py
@@ -1,43 +1,61 @@
 #Prints to console for user input
-# print("Enter which operation to continue ( *, /, +, - ): ")
-# x = input()
-
-
-
-# if x=="*":
-#     print("Print numbers you want to multiply: ")
-#     firstNum = input()
-#     secondNum = input() 
-#     float(firstNum) * float(secondNum)
-
-
-    
-
-
-
 
 def add():
     sum = 0
     num = 0
     while num != -1:
         sum += num
-        print("Enter numbers to add, enter -1 to exit") 
+        print("Enter numbers to add, type -1 to enter values") 
         num = float(input())    
 
     print(sum)  
 
+def multiply():
+    product = 0
+    num = 0
+    while num != -1:
+        product = num * num
+        print("Enter numbers to multiply, type -1 to enter values")
+        num = float(input())
 
+    print(product)
 
+def subtraction():
+    difference = 0
+    num = 0
+    while num != -1:
+        difference -= num
+        print("Enter numbers to subtract, type -1 to enter values")
+        num = float(input())
+
+    print(difference)
+
+def division():
+    quotient = 0
+    num = 0
+    while num != -1:
+        quotient = num / num
+        print("Enter numbers to divide, type -1 to enter values")
+        num = float(input())
+
+    print(quotient)
 
 
 
 print("Welcome to my awesome calculator")
 
 print("What would you like to calculate")
-print("Area of triangle, c to f, add, sub, divide, multiple")
+print("These are your options! Area of triangle, c to f, add, sub, divide, multiply")
 task = input()
 
-if task == "Area of triangle":
-    areaOfTriangle()
+if task == "multiply":
+    multiply()
 elif task == "add":
     add()
+elif task == "subtract":
+    subtraction()
+elif task == "divide":
+    division()
+else:
+    print("Enter a valid operation!")
+
