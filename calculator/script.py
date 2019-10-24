c3cb5702b676c48de60b95eaca73e2397f9a554a
Frank Di Giglio <fdigiglio22109@columbushs.com>
2019-10-23 21:04:02 -0400

    functional calculator with temp and basic operations

--- calculator/script.py
+++ calculator/script.py
@@ -1,61 +1,72 @@
 #Prints to console for user input
 
+#function for addition
 def add():
-    sum = 0
-    num = 0
-    while num != -1:
-        sum += num
-        print("Enter numbers to add, type -1 to enter values") 
-        num = float(input())    
+    firstNum = float(input())
+    secondNum = float(input())
+    sumNumbers = firstNum + secondNum
+    print(sumNumbers)
 
-    print(sum)  
-
-def multiply():
-    product = 0
-    num = 0
-    while num != -1:
-        product = num * num
-        print("Enter numbers to multiply, type -1 to enter values")
-        num = float(input())
-
-    print(product)
-
-def subtraction():
-    difference = 0
-    num = 0
-    while num != -1:
-        difference -= num
-        print("Enter numbers to subtract, type -1 to enter values")
-        num = float(input())
-
+#function for subtraction
+def subtract():
+    firstNum = float(input())
+    secondNum = float(input())
+    difference = firstNum - secondNum
     print(difference)
 
-def division():
-    quotient = 0
-    num = 0
-    while num != -1:
-        quotient = num / num
-        print("Enter numbers to divide, type -1 to enter values")
-        num = float(input())
+#function for multiplication
+def multiplication():
+    firstNum = float(input())
+    secondNum = float(input())
+    product = firstNum * secondNum
+    print(product)
 
+#function for division
+def division():
+    firstNum = float(input())
+    secondNum = float(input())
+    quotient = firstNum / secondNum
     print(quotient)
 
+#Temperature conversions
 
+#Celcius to Fahrenheit
+def celciusFahrenheit():
+    print("Enter Celcius Degrees: ")
+    c_read = input()
+    c_float = float(c_read)   
+    celcius =  float(c_float * 1.8 + 32.0)
+    print(str(celcius) + " Degrees Fahrenheit!")
 
-print("Welcome to my awesome calculator")
+#Fahrenheit to Celcius
+def fahrenheitCelcius():
+    print("Enter Fahrenheit Degrees: ")
+    f_read = input()
+    f_float = float(f_read)
+    fahrenheit =  float((f_float - 32.0) * 5/9)
+    print(str(fahrenheit) + " Degrees Celcius!")
 
-print("What would you like to calculate")
-print("These are your options! Area of triangle, c to f, add, sub, divide, multiply")
+#Prints command interface
+print("Welcome to my basic 4 Function Calculator!                ")
+print("What would you like to calculate today?                   ")
+print("These are your options! Add, Subtract, Multiply or Divide ")
+print("For tempertature conversions type f to c or c to f.       ")
+print("--------------------------------------------------------- ")
 task = input()
 
+#If statements that run certain functions if key is inputed
 if task == "multiply":
-    multiply()
+    multiplication()
 elif task == "add":
     add()
 elif task == "subtract":
-    subtraction()
+    subtract()
 elif task == "divide":
     division()
+elif task == "c to f":
+    celciusFahrenheit()
+elif task == "f to c":
+    fahrenheitCelcius()
 else:
-    print("Enter a valid operation!")
+    print("Enter a valid operation that is provided!")
 
