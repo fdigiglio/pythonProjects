bfc9f0e8f3f7b45f567859151115e490e21b9d6f
fdigiglio <54536680+fdigiglio@users.noreply.github.com>
2019-10-24 14:17:25 -0400

    terminalAdvGame

---
+++ terminalAdvGame/script.py
@@ -0,0 +1,17 @@
+
+playerLives = 5
+
+print("Welcome to the Terminal Adventure Game!")
+print("Controls for Yes is y and for No is n! ")
+print("---------------------------------------")
+
+decision = input()
+
+def firstLevel():
+    print("Welcome to Colombia where the date is, October 24, 1986. \n You are an employee for Mr. Escobar but are not sure to report him to the police. \n You are receiving a phone call now from him.")
+
+
+while playerLives > 0:
+    
+    if decision == "begin":
+        firstLevel()
