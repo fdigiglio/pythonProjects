12cb88d4af50832bcd9b0072b1dc787c52461976
Frank Di Giglio <fdigiglio22109@columbushs.com>
2019-10-24 21:32:22 -0400

    prompts

--- terminalAdvGame/script.py
+++ terminalAdvGame/script.py
@@ -9,9 +9,14 @@ decision = input()
 
 def firstLevel():
     print("Welcome to Colombia where the date is, October 24, 1986. \n You are an employee for Mr. Escobar but are not sure to report him to the police. \n You are receiving a phone call now from him.")
+    print("Type yes to pick up the phone, no to hangup.")
 
+def secondPrompt():
+    decision
 
-while playerLives > 0:
+#while playerLives > 0:
     
-    if decision == "begin":
-        firstLevel()
+if decision == "begin":
+    firstLevel()
+    if decision == "yes":
+        secondPrompt()
