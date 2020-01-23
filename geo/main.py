c1c9806a958c1626a30b1607c41fca74e03e98f7
Frank Di Giglio <fdigiglio22109@columbushs.com>
2020-01-22 20:23:57 -0500

    Finished

--- geo/main.py
+++ geo/main.py
@@ -1,7 +1,7 @@
 class Quadrilaterral():
 
     def __init__(self, sides):
-        self.sides = 4
+        self.sides = sides
 
     def display(self):
         print(self.sides)
@@ -15,31 +15,69 @@ class Parallelogram(Quadrilaterral):
         self.congruent_sides = congruent_sides
 
     def display(self):
-        print(self.sides)
-        print(self.parallel_lines)
-        print(self.congruent_angles)
-        print(self.congruent_sides)
+        print(self.sides, "Sides")
+        print(self.parallel_lines, "Parallel lines")
+        print(self.congruent_angles, "Congruent angles")
+        print(self.congruent_sides, "Congruent sides")
 
-    
+    def checkShape(self):
+        if self.sides == 4:
+            print("This is a QUADRILATERAL!")
+        elif self.sides < 4 or self.sides > 4:
+            print("This is not a quadrilateral but IS a POLYGON")
+        if self.sides == 4 and self.parallel_lines == 4:
+            print("PARALLELOGRAM!")
+        if self.sides == 4 and self.parallel_lines == 4 and self.congruent_angles == 4:
+            print("RECTANGLE!")
+        if self.sides == 4 and self.parallel_lines == 4 and self.congruent_sides == 4:
+            print("RHOMBUS")
+        if self.sides == 4 and self.parallel_lines == 4 and self.congruent_angles == 4 and self.congruent_sides == 4:
+            print("SQUARE!")
         
-
-
 
 class Polygon(Quadrilaterral):
     def __init__(self, sides, parallel_lines, congruent_angles, congruent_sides):
-        super().__init__()
+        super().__init__(sides)
+        self.parallel_lines = parallel_lines
+        self.congruent_angles = congruent_angles
+        self.congruent_sides = congruent_sides
+    def checkShape(self):
+        if self.sides == 4:
+            print("This is a QUADRILATERAL!")
+        elif self.sides < 4 or self.sides > 4:
+            print("This is not a quadrilateral but IS a POLYGON")
+        if self.sides == 4 and self.parallel_lines == 2:
+            print("Trapezoid")
+        if self.sides == 4 and self.parallel_lines == 2 and self.congruent_sides == 2:
+            print("Isosceles Trapezoid!")
+        if self.sides == 4 and self.parallel_lines == 0 and self.congruent_sides == 4:
+            print("KITE!")
 
 
+rectangle = Parallelogram(4, 4, 4, 0)
+rectangle.checkShape()
 
+print("======================================================")
 
+rhombus = Parallelogram(4, 4, 0, 4)
+rhombus.checkShape()
 
-z = Parallelogram(4, 4, 4, 2)
-print("This Parallelogram has", z.sides, "sides,", z.parallel_lines, "parallel lines,", z.congruent_angles, "congruent angles,", z.congruent_sides, "congruent sides.")
-print(z.parallel_lines)
-z.display()
+print("======================================================")
 
-#write if and else statement checking if it has the specific needed amount of properties in order to be a parallelogram or just a polygon that is also a quadrilaterall        
+square = Parallelogram(4,4,4,4)
+square.checkShape()
 
+print("======================================================")
 
-if z.parallel_lines == 4 and z.congruent_angles == 4:
-    print("This is a rectangle")
+trapezoid = Polygon(4, 2, 0, 0)
+trapezoid.checkShape()
+
+print("======================================================")
+
+isoTrap = Polygon(4, 2, 0, 2)
+isoTrap.checkShape()
+
+print("======================================================")
+
+kite = Polygon(4, 0, 0, 4)
+kite.checkShape()
